d10226338240f0e0af619f8e71c7f8df4ea935d5
vavoccia <vvoccia@outlook.com>
2021-07-26 09:38:00 -0400

    Updated render and api for testing

--- api/api.js
+++ api/api.js
@@ -1,12 +1,12 @@
 fs = require("fs");
 exports.gridImages = (req, res) => {
-  fs.readdir(`${__dirname}\\..\\assets\\grid-images`, (err, files) => {
+  fs.readdir(`${__dirname}`, (err, files) => {
     console.log(__dirname);
     if (err) console.log(err);
     else {
       res.send(
         files.filter((file) => {
-          return file.includes(".jpg");
+          return file.includes("*.*");
         })
       );
     }
