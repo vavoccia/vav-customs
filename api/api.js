9b9356c099d4965662601a010d5d8ed3836bedd9
vavoccia <vvoccia@outlook.com>
2021-07-26 07:01:25 -0400

    Updates to package.json

--- api/api.js
+++ api/api.js
@@ -1,12 +1,14 @@
-fs = require ("fs");
+fs = require("fs");
 exports.gridImages = (req, res) => {
-    fs.readdir(`${__dirname}\\..\\assets\\grid-images`, (err, files) => {
-        console.log(__dirname);
-        if (err) console.log(err);
-        else {
-          res.send(files.filter( (file) => {
-              return file.includes(".jpg");
-          })
-        )};
-    })
+  fs.readdir(`${__dirname}\\..\\assets\\grid-images`, (err, files) => {
+    console.log(__dirname);
+    if (err) console.log(err);
+    else {
+      res.send(
+        files.filter((file) => {
+          return file.includes(".jpg");
+        })
+      );
+    }
+  });
 };
