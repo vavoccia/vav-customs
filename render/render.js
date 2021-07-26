95f81e1b72c55889d5c72987e1f33338fa1dc2e2
vavoccia <vvoccia@outlook.com>
2021-07-26 10:38:21 -0400

    updated endpoint path

--- render/render.js
+++ render/render.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 exports.homePage = (req, res) => {
   axios
-    .get(`https://vav-customs.azurewebsites.net/api/grid-images`)
+    .get(`/api/grid-images`)
     .then((response) => {
       res.render("index", { files: response.data });
     })
